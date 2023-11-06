4a7187e2de6fc8da00f87656f8e2881f818adb7f
afonso-iscte <astaa@iscte-iul.pt>
2023-11-06 13:44:11 +0000

    5-8 Classes Adjacentes

---
+++ Sala.java
@@ -0,0 +1,20 @@
+package projeto_es;
+
+public class Sala {
+
+	private String sala;
+	
+	public Sala(String sala) {
+		this.sala = sala;
+	}
+	
+	public String getSala() {
+		return this.sala;
+	}
+	
+	@Override
+	public String toString() {
+		return this.sala;
+	}
+	
+}
